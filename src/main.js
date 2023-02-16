12c1c0affe20a67536d4ff0c0a6cbd545e6a18a2
Oleksandr Dubin <102242242+AlexDubin@users.noreply.github.com>
2023-02-16 17:28:54 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// JS Hope page
+
+import './js/kravchuk_about_restaurant';
+import './js/orest_details_restaurant';
+import './js/dubin_about_hotel';
+import './js/tetiana_details_hotel';
+
+// JS modal
+
+import './js/gala_modal_price';
+import './js/orest_modal_table';
+import './js/tetiana_modal_room';
+
+// JS Hotel
+
+import './js/sukharieva_rooms';
+import './js/gala_triple';
+
+// JS Rooms
+
+import './js/valentii_room_hero';
