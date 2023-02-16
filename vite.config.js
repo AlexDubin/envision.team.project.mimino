7b820ffba99d0fd442f62ac4feed831293d7a1bc
Oleksandr Dubin <102242242+AlexDubin@users.noreply.github.com>
2023-02-16 17:43:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
-        restourant: resolve(__dirname, 'src/restaurant'),
+        restourant: resolve(__dirname, 'src/restaurant.html'),
         hotel: resolve(__dirname, 'src/hotel.html'),
         lux: resolve(__dirname, 'src/room_lux.html'),
         double: resolve(__dirname, 'src/room_double.html'),
