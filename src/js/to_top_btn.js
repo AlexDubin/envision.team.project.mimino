41ee0aa900d25dd5a911692b885bc0bc31e57a52
Acidhouzee <Bohdankireev@gmail.com>
2023-02-22 20:45:24 +0200

    go to top btn

---
+++ src/js/to_top_btn.js
@@ -0,0 +1,26 @@
+const button = document.getElementById('scrollToTopBtn');
+
+const displayButton = () => {
+  window.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+  
+    if (window.scrollY > 100) {
+      button.classList.add('show-btn');
+    } else {
+      button.classList.add('remove-btn');
+    }
+  });
+};
+
+const scrollToTop = () => {
+  button.addEventListener("click", () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    }); 
+  });
+};
+
+displayButton();
+scrollToTop();
